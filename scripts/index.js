4d6b55613182062a49b9ef1c3f5026a2907b3704
Makar Minaev <minaev1makar@yandex.ru>
2024-12-05 18:12:15 +0300

    первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+function createCard(data, deleteCard) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    cardElement.querySelector(".card__image").src = data.link;
+    cardElement.querySelector(".card__title").textContent = data.name;
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", () => {
+        deleteCard(cardElement);
+    });
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+function renderCards(initialCards) {
+    const cardsContainer = document.querySelector(".places__list");
+    initialCards.forEach((cardData) => {
+      const card = createCard(cardData, deleteCard);
+      cardsContainer.append(card);
+    });
+}
+
+renderCards(initialCards);
+
+
+
+
+
+
+
+
+
